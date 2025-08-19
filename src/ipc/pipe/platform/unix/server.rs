023abf56b7edfd91649bcdc1372f162ded15d7fb
vyfor <vyforium@gmail.com>
2025-08-20 04:57:47 +0500

    .

--- src/ipc/pipe/platform/unix/server.rs
+++ src/ipc/pipe/platform/unix/server.rs
@@ -11,7 +11,7 @@ use crate::ipc::pipe::{PipeClientImpl, PipeServerImpl};
 use crate::messages::events::local::ErrorEvent;
 use crate::messages::message::Message;
 use crate::session::SessionManager;
-use crate::{client_event, echo, local_event};
+use crate::{client_event, echoln, local_event};
 
 pub struct PipeServer {
     session_manager: Arc<SessionManager>,
